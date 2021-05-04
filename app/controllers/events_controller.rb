7a6ba576706d539a67506f90aed39a9f61643214
Takuya-Endo <above5purple@gmail.com>
2021-05-04 02:54:51 +0000

    [Fix]appointmentsコントローラ

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,9 +2,11 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all
-    @appointment = Appointment.all
-    @meeting = Meeting.all
-    @task = Task.all
+    # start_date = params.fetch(:start_date, Date.today).to_date
+    # end_date = params.fetch(:end_date, Date.today).to_date
+    # @events = Event.where(start_date: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week, end_date: end_date.beginning_of_month.beginning_of_week..end_date.end_of_month.end_of_week)
+
+    # @events = Event.where(end_date: end_date.beginning_of_month.beginning_of_week..end_date.end_of_month.end_of_week)
   end
 
   def new
