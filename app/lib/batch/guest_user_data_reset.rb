e52e92dc747bdb31e507676f4fe9c84d27463967
Takuya-Endo <above5purple@gmail.com>
2021-06-19 06:29:15 +0000

    [Fix]guest_user_data_reset仕事の全削除

--- app/lib/batch/guest_user_data_reset.rb
+++ app/lib/batch/guest_user_data_reset.rb
@@ -1,7 +1,7 @@
 class Batch::GuestUserDataReset
   def self.guest_user_data_reset
 
-    Event.where(user_id: 3).delete_all # FIXME:ネストされたtasksが削除されていない
+    Event.where(user_id: 3).destroy_all # FIXME:ネストされたtasksが削除されていない
     p "ゲストユーザーの仕事を全て削除しました"
 
     # TODO:今週の火曜日に資料作成の予定をサンプルとして追加するバッチ処理
