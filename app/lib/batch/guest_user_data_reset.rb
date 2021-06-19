67c410802ff6efc593bb2fab1ea6789659d5becc
Takuya-Endo <above5purple@gmail.com>
2021-06-19 05:22:21 +0000

    [Update]guest_user_data_resetコメントアウト

--- app/lib/batch/guest_user_data_reset.rb
+++ app/lib/batch/guest_user_data_reset.rb
@@ -1,6 +1,13 @@
 class Batch::GuestUserDataReset
   def self.guest_user_data_reset
-    Event.where(user_id: 3).delete_all
+
+    Event.where(user_id: 3).delete_all # FIXME:ネストされたtasksが削除されていない
     p "ゲストユーザーの仕事を全て削除しました"
+
+    # TODO:今週の火曜日に資料作成の予定をサンプルとして追加するバッチ処理
+    # Event.create(user_id: 3)
+    # Task.create(deadline_status: 2, title: "資料作成" start_date: Date.today.beginning_of_week + 1.day)
+    # p "ゲストユーザーのサンプルtaskを追加しました"
+
   end
 end
